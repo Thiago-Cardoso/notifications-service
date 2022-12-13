57957358a5d847da6893521919fbe9c1b7650598
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2022-12-13 20:01:11 -0300

    chore: structure and configuration of prisma

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { HttpModule } from './http.module';
+import { PrismaService } from './prisma.service';
+
+@Module({
+  imports: [HttpModule],
+  controllers: [AppController],
+  providers: [PrismaService],
+})
+export class AppModule {}
---
+++ src/http.module.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Module } from '@nestjs/common';
+
+@Module({
+  imports: [],
+})
+export class HttpModule {}
